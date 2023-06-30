486dae8858f70fbcddc4fa191c6272f748eaec72
John Denlinger <john_d@dc3.jp>
2023-06-30 15:29:28 +0900

    prevent error when featured image doesn't exist

--- next.config.js
+++ next.config.js
@@ -4,13 +4,30 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
+        protocol: "https",
+        hostname: "dc3common2.sakura.ne.jp",
         port: "",
-        pathname: "/headless-cms/**",
+        pathname: "/dc3defo/**",
       },
     ],
   },
 };
 
 module.exports = nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "http",
+//         hostname: "localhost",
+//         port: "",
+//         pathname: "/headless-cms/**",
+//       },
+//     ]
+//   },
+// };
+
+// module.exports = nextConfig;
