ac7f62b448b5b5040e39a78805f93371f6bbb22e
John Denlinger <john_d@dc3.jp>
2023-07-19 09:09:31 +0900

    add global styles

--- components/Nav/index.tsx
+++ components/Nav/index.tsx
@@ -1,24 +1,49 @@
 import Link from "next/link";
+import Image from "next/image";
+
+import logo from "../../assets/images/logo.png";
 
 export const Nav = () => {
   return (
-    <header className="container mx-auto py-4 px-4">
-      <div className="navbar w-full md:w-3/5 mx-auto border-b-2">
-        <div className="flex items-center justify-center pb-4 text-base md:text-xl">
-          <Link href="/" className="px-4">
+<header className="header">
+<div className="headerflex">
+    <div className="left">
+        <h1>
+          <Link href="/">
+            <Image 
+            src={logo}
+            alt="">
+            </Image>
+            </Link></h1>
+    </div>
+    <div className="right">   
+    <nav className="gnav">
+      <ul className="nonstyle">
+        <li>      <Link href="/" className="px-4">
             Home
-          </Link>
-          <Link href="#" className="px-4">
+          </Link></li>
+          <li>          <Link href="#" className="px-4">
             Blog
-          </Link>
+          </Link></li>
+          <li>
           <Link href="#" className="px-4">
             About
-          </Link>
-          <Link href="#" className="px-4">
+          </Link></li>
+          <li>          
+            <Link href="#" className="px-4">
             Contact
-          </Link>
-        </div>
-      </div>
-    </header>
+          </Link></li>
+        </ul></nav> 
+        <nav className="sns pcblock">
+            <ul className="ul_sns nonstyle">
+                <li><a href="" target="_blank" className="fb"><i className="fab fa-facebook-f"></i></a></li>
+                <li><a href="" target="_blank" className="tw"><i className="fab fa-twitter"></i></a></li>
+                <li><a href="" target="_blank" className="in"><i className="fab fa-instagram"></i></a></li>
+                <li><a href="" target="_blank" className="yt"><i className="fab fa-youtube"></i></a></li>
+            </ul>
+        </nav>
+    </div>
+</div>
+</header>
   );
 };
--- next.config.js
+++ next.config.js
@@ -19,6 +19,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
         protocol: "http",
